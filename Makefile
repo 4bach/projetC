fb44a745423e7809011e1d863b98688893cd60f2
KARMIM YANNIS <3414093@ppti-14-403-14.ufr-info-p6.jussieu.fr>
2017-02-21 18:07:45 +0100

    Semaine1 V2

---
+++ Makefile
@@ -0,0 +1,21 @@
+﻿GCC_FLAGS = -Wall
+all: main 
+
+entree_sortie.o	: entree_sortie.c
+	gcc $(GCC_FLAGS) -c entree_sortie.c
+
+Chaine.o: Chaine.c
+	gcc $(GCC_FLAGS) -c Chaine.c
+
+
+main.o	: main.c
+	gcc $(GCC_FLAGS) -c main.c
+
+main		: Chaine.o main.o entree_sortie.o
+	gcc $(GCC_FLAGS) -o main main.o Chaine.o entree_sortie.o 
+
+clean	:
+	rm -f entree_sortie.o Chaine.o  main.o main 
+
+
+
---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Chaine.h"
+
+void menu(){
+	
+	
+	printf("1: LECTURE DU FICHIER\n");
+	printf("1: LECTURE DU FICHIER\n");
+	printf("1: LECTURE DU FICHIER\n");
+	printf("1: LECTURE DU FICHIER\n");
+	printf("1: LECTURE DU FICHIER\n");
+	
+}
+
+
+
+int main(int argc, char* *argv){
+	int ch;
+	char* nomfic=strdup(argv[1]);
+	if(argc!=3)
+		return 1;
+	FILE* f=fopen(nomfic,"r");
+	if(f==NULL){
+		printf("Le fichier n'a pas pu être ouvrir\n");
+		return 2;
+	}
+	do{
+
+		menu();
+		scanf("%d",&ch);
+
+		switch(ch){
+
+			case 1:
+				lectureChaine(f);
+				printf("Lecture\n");
+				
+				break;
+
+
+
+		}
+
+
+
+
+	}while(ch!=0);
+
+	printf("FIN\n");
+	return 0;
+	
+}
