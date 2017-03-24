ec65bedb61006db6d1d41da6930d31c16a2a4a20
jeandeb <johnydebod@gmail.com>
2017-03-24 16:07:22 +0100

    Arbres Commit

--- Arbres.c
+++ Arbres.c
@@ -8,17 +8,15 @@
 #include "entree_sortie.h"
 #include "Arbre.h"
 
-
-
 void chaineCoordMinMax(Chaines* C, double* minx, double* miny, double* maxx, double* maxy)
 {
 	CellChaine* courant=C->chaines;
 	CellPoint* cpoint=NULL;
 
 	*minx=100;
-	*miny=100;
+	*miny=0;
 	*maxx=0;
-	*maxy=0;
+	*maxy=100;
 
 	while(courant){
 		
@@ -51,7 +49,6 @@ void afficheArbre( ArbreQuat* a )
 	afficheArbre( a->no );
 	afficheArbre( a->no );
 }
-
 ArbreQuat* creerArbreQuat(double xc, double yc, double coteX, double coteY)
 {
 	ArbreQuat* arbreQuat = ( ArbreQuat* )malloc( sizeof( ArbreQuat ) );
@@ -70,120 +67,146 @@ ArbreQuat* creerArbreQuat(double xc, double yc, double coteX, double coteY)
 ArbreQuat* orientationArbre( double x, double y, ArbreQuat* a )
 {
 	ArbreQuat* arbre;
-	if( ( x > a->xc ) && ( y > a->yc ) ) {
-			arbre =  a->ne;
-		}
+	if( ( x >= a->xc ) && ( y >= a->yc ) ) {
+		arbre =  a->ne;
+	}
 		//SUD EST
-		else if ( ( x > a->xc ) && ( y < a->yc ) ) {
-			arbre = a->se;
-		}
+	else if ( ( x >= a->xc ) && ( y <= a->yc ) ) {
+		arbre = a->se;
+	}
 		//SUD OUEST
-		else if ( ( x < a->xc ) && ( y < a->yc ) ) {
-			arbre = a->so;
-		}
+	else if ( ( x <= a->xc ) && ( y <= a->yc ) ) {
+		arbre = a->so;
+	}
 		//NORD OUEST 
-		else if ( ( x < a->xc ) && ( y > a->yc ) ) {
-			arbre = a->no;
-		}
-		return arbre;
+	else if ( ( x <= a->xc ) && ( y >= a->yc ) ) {
+		arbre = a->no;
+	}
+	return arbre;
 }
 
 ArbreQuat* insererNoeudArbre(Noeud* n, ArbreQuat* a, ArbreQuat* parent)
 {
-	
 	if( !a ) {
-		int coteX = parent->coteX/2;
-		int coteY = parent->coteY/2;
-		int yc, xc;
-		printf( "3 2 1 1\n" );
+		double xc, yc;
+		double coteXnew = parent->coteX/2;
+		double coteYnew = parent->coteY/2;
 
-		//NORD EST
-		if( ( n->x > parent->xc ) && ( n->y > parent->yc ) ) {
-			yc = parent->yc + (1/2)*(coteY - parent->yc);
-			xc = parent->xc + (1/2)*(coteX - parent->xc);
-			a = creerArbreQuat( xc, yc, coteX, coteY );
-			parent->ne = a;
-			a->noeud = n;
+		if( ( n->x >= parent->xc ) && ( n->y >= parent->yc ) ) {
+			printf( "test 1 vrai\n" );
+			yc = parent->yc + (1./2.)*(parent->coteY - parent->yc);
+			xc = parent->xc + (1./2.)*(parent->coteX - parent->xc);
 		}
-		//SUD EST
-		else if ( ( n->x > parent->xc ) && ( n->y < parent->yc ) ) {
-			yc = parent->yc - (1/2)*(coteY - parent->yc);
-			xc = parent->xc + (1/2)*(coteX - parent->xc);
-			a = creerArbreQuat( xc, yc, coteX, coteY );
-			parent->se = a;
-			a->noeud = n;
+
+			//SUD EST
+		else if ( ( n->x >= parent->xc ) && ( n->y <= parent->yc ) ) {
+			printf( "test 2 vrai\n" );
+			yc = parent->yc - (1./2.)*(parent->coteY - parent->yc);
+			xc = parent->xc + (1./2.)*(parent->coteX - parent->xc);
+		
 		}
 		//SUD OUEST
-		else if ( ( n->x < parent->xc ) && ( n->y < parent->yc ) ) {
-			yc = parent->yc - (1/2)*(coteY - parent->yc);
-			xc = parent->xc - (1/2)*(coteX - parent->xc);
-			a = creerArbreQuat( xc, yc, coteX, coteY );
-			parent->so = a;
-			a->noeud = n;
+		else if ( ( n->x <= parent->xc ) && ( n->y <= parent->yc ) ) {
+			printf( "test 3 vrai\n" );
+			yc = parent->yc - (1./2.)*(parent->coteY - parent->yc);
+			xc = parent->xc - (1./2.)*(parent->coteX - parent->xc);
+		
 		}
 		//NORD OUEST 
-		else if ( ( n->x < parent->xc ) && ( n->y > parent->yc ) ) {
-			yc = parent->yc + (1/2)*(coteY - parent->yc);
-			xc = parent->xc - (1/2)*(coteX - parent->xc);
-			a = creerArbreQuat( xc, yc, coteX, coteY );
-			parent->no = a;
-			a->noeud = n;
+		else if ( ( n->x <= parent->xc ) && ( n->y >= parent->yc ) ) {
+			printf( "test 4 vrai\n" );
+			yc = parent->yc + (1./2.)*(parent->coteY - parent->yc);
+			xc = parent->xc - (1./2.)*(parent->coteX - parent->xc);
+			printf( "test4 XC = %f, YC =%f \n", xc, yc );
+				
 		}
+		a = creerArbreQuat( xc, yc, coteXnew, coteYnew );
+		a->noeud = n;
 
-		return parent;
-
+		return a;
 	}
 	else if( a->noeud ) {
 		
-
 		Noeud* n_tmp = a->noeud;
 		a->noeud = NULL;
-		printf( "3 2 1\n" );
-
-		a = insererNoeudArbre( n_tmp, a , parent );
-
-		printf( "3 2 2\n" );
-
-		a = insererNoeudArbre( n, a, parent );
-		return parent;
-		
-		
-		
-	}
-	//A PRECISER(CEST JUSTE UNE COPIE DE CELUI DE DESSUS)
-	else if( !a->noeud && a ) {
-		printf( "3 2 3\n" );
-		if( ( n->x > a->xc ) && ( n->y > a->yc ) ) {
-			a = insererNoeudArbre( n, a->ne ,a );
+		if( ( n_tmp->x >= a->xc ) && ( n_tmp->y >= a->yc ) ) {
+			a->ne = insererNoeudArbre( n_tmp, a->ne ,a );
 
 		}
 		//SUD EST
-		else if ( ( n->x > a->xc ) && ( n->y < a->yc ) ) {
-			a = insererNoeudArbre( n, a->se ,a );
+		else if ( ( n_tmp->x >= a->xc ) && ( n_tmp->y <= a->yc ) ) {
+			a->se = insererNoeudArbre( n_tmp, a->se ,a );
 
 		}
 		//SUD OUEST
-		else if ( ( n->x < a->xc ) && ( n->y < a->yc ) ) {
-			a = insererNoeudArbre( n, a->so ,a );
+		else if ( ( n_tmp->x <= a->xc ) && ( n_tmp->y <= a->yc ) ) {
+			a->so = insererNoeudArbre( n_tmp, a->so ,a );
 
 		}
 		//NORD OUEST 
-		else if ( ( n->x < a->xc ) && ( n->y > a->yc ) ) {
-			a = insererNoeudArbre( n, a->no, a );
+		else if ( ( n_tmp->x <= a->xc ) && ( n_tmp->y >= a->yc ) ) {
+			a->no = insererNoeudArbre( n_tmp, a->no, a );
 
 		}
+		if( ( n->x >= a->xc ) && ( n->y >= a->yc ) ) {
+			a->ne = insererNoeudArbre( n, a->ne ,a );
+
+		}
+		//SUD EST
+		else if ( ( n->x >= a->xc ) && ( n->y <= a->yc ) ) {
+			a->se = insererNoeudArbre( n, a->se ,a );
+
+		}
+		//SUD OUEST
+		else if ( ( n->x <= a->xc ) && ( n->y <= a->yc ) ) {
+			a->so = insererNoeudArbre( n, a->so ,a );
+
+		}
+		//NORD OUEST 
+		else if ( ( n->x <= a->xc ) && ( n->y >= a->yc ) ) {
+			a->no = insererNoeudArbre( n, a->no, a );
+
+		}
+		return a;
+
 	}
-	return parent;
+
+	if( ( n->x >= a->xc ) && ( n->y >= a->yc ) ) {
+		a->ne = insererNoeudArbre( n, a->ne ,a );
+
+	}
+	//SUD EST
+	else if ( ( n->x >= a->xc ) && ( n->y <= a->yc ) ) {
+		a->se = insererNoeudArbre( n, a->se ,a );
+
+	}
+	//SUD OUEST
+	else if ( ( n->x <= a->xc ) && ( n->y <= a->yc ) ) {
+		a->so = insererNoeudArbre( n, a->so ,a );
+
+	}
+	//NORD OUEST 
+	else if ( ( n->x <= a->xc ) && ( n->y >= a->yc ) ) {
+		a->no = insererNoeudArbre( n, a->no, a );
+
+	}
+	return a;
+	
+
 }
 
 Noeud* chercherNoeudArbre(CellPoint* pt, Reseau* R, ArbreQuat** aptr, ArbreQuat* parent)
 {
 	Noeud* n;
+	ArbreQuat* tmp_aptr = *aptr;
+	ArbreQuat* tmp;
 	if( *aptr == NULL ) {
-		printf( "3 1\n" );
+		printf( "*aptr == NULL\n" );
 		n = creerNoeud( R, pt->x, pt->y );
-		parent = insererNoeudArbre( n, *aptr, parent );
+
+		*aptr = insererNoeudArbre( n, *aptr, parent );
 		
+
 		CellNoeud* Cl = creerCellNoeud( n );
 		Cl->suiv=R->noeuds;
 		R->noeuds=Cl;
@@ -196,9 +219,9 @@ Noeud* chercherNoeudArbre(CellPoint* pt, Reseau* R, ArbreQuat** aptr, ArbreQuat*
 			printf("test vrai\n");
 			return (*aptr)->noeud;
 		}
-		printf( "3 2\n" );
+		printf( "(*aptr)->noeud != NULL\n" );
 		n = creerNoeud( R, pt->x, pt->y );
-		parent = insererNoeudArbre( n, *aptr, parent );
+		(*aptr) = insererNoeudArbre( n, *aptr, parent );
 
 		CellNoeud* Cl = creerCellNoeud( n );
 		Cl->suiv=R->noeuds;
@@ -207,27 +230,28 @@ Noeud* chercherNoeudArbre(CellPoint* pt, Reseau* R, ArbreQuat** aptr, ArbreQuat*
 		return n;
 	}
 	else if ( !(*aptr)->noeud && (*aptr) ){
-		printf( "3 3\n" );
-		if( ( pt->x > (*aptr)->xc ) && ( pt->y > (*aptr)->yc ) ) {
+		
+		if( ( pt->x >= (*aptr)->xc ) && ( pt->y >= (*aptr)->yc ) ) {
 
 			n = chercherNoeudArbre( pt, R, &(*aptr)->ne, (*aptr));
 		}
 		//SUD EST
-		else if ( ( pt->x > (*aptr)->xc ) && ( pt->y < (*aptr)->yc ) ) {
+		else if ( ( pt->x >= (*aptr)->xc ) && ( pt->y <= (*aptr)->yc ) ) {
 
 			n = chercherNoeudArbre( pt, R, &(*aptr)->se, *aptr);
 		}
 		//SUD OUEST
-		else if ( ( pt->x < (*aptr)->xc ) && ( pt->y < (*aptr)->yc ) ) {
+		else if ( ( pt->x <= (*aptr)->xc ) && ( pt->y <= (*aptr)->yc ) ) {
 
 			n = chercherNoeudArbre( pt, R, &(*aptr)->so, (*aptr) );
 		}
 		//NORD OUEST 
-		else if ( ( pt->x < (*aptr)->xc ) && ( pt->y > (*aptr)->yc ) ) {
+		else if ( ( pt->x <= (*aptr)->xc ) && ( pt->y >= (*aptr)->yc ) ) {
 
 			n = chercherNoeudArbre( pt, R, &(*aptr)->no, (*aptr) );
 		}
 	}
+	*aptr = tmp_aptr;
 	return n;
 }
 
@@ -236,8 +260,12 @@ Reseau* recreeReseauArbre(Chaines* C)
 
 	double minx, miny, maxx, maxy;
 	chaineCoordMinMax( C, &minx, &miny, &maxx, &maxy );
-	double coteX = maxx - minx;
-	double coteY = maxy - miny;
+	//double coteX = maxx - minx;
+	//double coteY = maxy - miny;
+	double coteX = 100;
+	double coteY = 100;
+	//printf( "maxx = %f, minx = %f, miny = %f, maxy = %f", maxx, minx, miny, minx );
+	//return NULL;
 	ArbreQuat* parent = creerArbreQuat( coteX/2, coteY/2, coteX, coteY );
 	ArbreQuat* enfant = NULL;
 	
@@ -255,19 +283,41 @@ Reseau* recreeReseauArbre(Chaines* C)
 		cpoint=courant->points;
 		courantco=(CellCommodite*)malloc(sizeof(CellCommodite));
 		while(cpoint){
-				
-			if(bool==0){
 
+			if( (cpoint->x>=parent->xc ) && (cpoint->y>=parent->yc ) ) {
+				enfant = parent->ne;
+			}
+				//SUD EST
+			else if ( (cpoint->x>=parent->xc ) && (cpoint->y<=parent->yc ) ) {
+				enfant =parent->se;
+			}
+				//SUD OUEST
+			else if ( (cpoint->x<=parent->xc ) && (cpoint->y<=parent->yc ) ) {
+				enfant =parent->so;
+			}
+				//NORD OUEST 
+			else if ( (cpoint->x<=parent->xc ) && (cpoint->y>=parent->yc ) ) {
+				enfant =parent->no;
+			}
+			if(bool==0){
+				
+				if( enfant == NULL ) printf( "enfant 1null\n" );
 				noeudCourant = chercherNoeudArbre( cpoint, R, &enfant, parent);
+				enfant = NULL;
+				//if( enfant == NULL ) printf( "enfant 2null\n" );
 				if( noeudCourant == NULL ) printf("noeud null\n");
 				else printf( "%f %f\n",noeudCourant->x, noeudCourant->y );
 
 				courantco->extrA=noeudCourant;
 				bool++;
-		
+
 			}
 			else{
+		
+				if( enfant == NULL ) printf( "enfant 1null\n" );
 				noeudCourant = chercherNoeudArbre( cpoint, R, &enfant, parent);
+				enfant = NULL;
+				//if( enfant == NULL ) printf( "enfant 2null\n" );
 				if( noeudCourant == NULL ) printf("noeud null\n");
 				else printf( "%f %f\n",noeudCourant->x, noeudCourant->y );
 				CellNoeud * cellPrec = (CellNoeud *) malloc(sizeof(CellNoeud));
@@ -299,3 +349,17 @@ Reseau* recreeReseauArbre(Chaines* C)
 	printf( "-----FINARBRE----\n" );
 	return R;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
